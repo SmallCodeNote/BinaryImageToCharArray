7b933af1501750acf7c9f349833639382fe25b63
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2023-05-07 19:20:10 +0900

    Add files via upload

---
+++ BinConverter/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace BinConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Run = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Run
+            // 
+            this.button_Run.Location = new System.Drawing.Point(12, 12);
+            this.button_Run.Name = "button_Run";
+            this.button_Run.Size = new System.Drawing.Size(98, 20);
+            this.button_Run.TabIndex = 0;
+            this.button_Run.Text = "button_Run";
+            this.button_Run.UseVisualStyleBackColor = true;
+            this.button_Run.Click += new System.EventHandler(this.button_Run_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(12, 169);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(577, 780);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("ＭＳ Ｐゴシック", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(577, 125);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 961);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Run;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ BinConverter/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace BinConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Run_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() != DialogResult.OK) return;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = ofd.FileName + ".c";
+
+
+
+            //if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            string filepass = ofd.FileName;
+
+            FileStream fs = new FileStream(filepass,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.None);
+
+            //バイナリ形式でプログラム内に読み込む
+            BinaryReader br = new BinaryReader(fs);
+
+            byte[] data;    //データを格納する変数
+
+            //ファイルサイズを求める
+            FileInfo fi = new FileInfo(filepass);
+            long filesize = fi.Length;
+
+            //配列の長さをファイルサイズにして定義
+
+            data = new byte[filesize];
+
+            //１バイトずつ取得しながら１６進数で表示
+
+            List<string> Lines = new List<string>();
+
+            int cnt = 1;
+
+            for (long i = 1; i < filesize; i++)
+            {
+                data[i] = br.ReadByte();
+                //Console.Write("{0,2:x} ", data[i]);
+                //25個表示したら改行
+                if (cnt == 25)
+                {
+                    Lines.Add(string.Format("0x{0,0:x2}\r\n", data[i]));
+                    cnt = 0;
+                }
+                else
+                {
+                    Lines.Add(string.Format("0x{0,0:x2}", data[i]));
+
+
+
+                }
+                cnt++;
+            }
+
+            br.Close();
+            br.Dispose();
+
+            Bitmap img = new Bitmap(ofd.FileName);
+
+            textBox1.Text =
+                "/// @brief ImageSize = "
+                + img.Width.ToString() +","+img.Height.ToString()+"\r\n"
+                + "unsigned char "
+                + Path.GetFileNameWithoutExtension(ofd.FileName)
+                +"[" + filesize.ToString() + "]=\r\n{"
+                +string.Join(",", Lines)
+                + "\r\n};"
+                ;
+
+            img.Dispose();
+
+            textBox2.Text += "extern unsigned char "
+                + Path.GetFileNameWithoutExtension(ofd.FileName)
+                + "[" + filesize.ToString() + "];\r\n";
+
+            Clipboard.SetText(textBox1.Text);
+
+        }
+    }
+}
